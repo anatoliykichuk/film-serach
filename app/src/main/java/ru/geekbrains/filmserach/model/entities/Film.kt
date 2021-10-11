18cf243d02f8d75a1c9237f8823511633c5496ab
Анатолий Кичук <anatoliy.kichuk@gmail.com>
2021-10-11 11:13:44 +0300

    Частично создана разметка списка.

---
+++ app/src/main/java/ru/geekbrains/filmserach/model/entities/Film.kt
@@ -0,0 +1,18 @@
+package ru.geekbrains.filmserach.model.entities
+
+data class Film(
+    val id: Int = 0,
+    val title: String,
+    val originalTitle: String = "",
+    val originalLanguage: String = "",
+    val genreIds: List<Int> = listOf<Int>(),
+    val releaseDate: String = "",
+    val adult: Boolean = false,
+    val overview: String = "",
+    val video: Boolean = false,
+    val popularity: Float = 0.0F,
+    val voteCount: Int = 0,
+    val voteAverage: Float = 0.0F,
+    val posterPath: String? = "",
+    val backdropPath: String? = "") {
+}
