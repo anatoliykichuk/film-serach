ec913017e7aa904392651cbd9e0201b89f91b0cd
anatoliy <anatoliy.kichuk@gmail.comgit config --global user.email anatoliy.kichuk@gmail.comgit config --global user.email anatoliy.kichuk@gmail.com>
2022-09-13 21:14:43 +0300

    добавлен список жанров

---
+++ app/src/main/java/ru/geekbrains/filmserach/model/entities/Constants.kt
@@ -0,0 +1,38 @@
+package ru.geekbrains.filmserach.model.entities
+
+fun getAllGenres(): Map<String, String> {
+    return mapOf(
+            "boevik" to "боевик",
+            "fentezi" to "фэнтези",
+            "fantastika" to "фантастика",
+            "triller" to "триллер",
+            "voennyj" to "военный",
+            "detektiv" to "детектив",
+            "komediya" to "комедия",
+            "drama" to "драма",
+            "uzhasy" to "ужасы",
+            "kriminal" to "криминал",
+            "melodrama" to "мелодрама",
+            "vestern" to "вестерн",
+            "biografiya" to "биография",
+            "anime" to "аниме",
+            "detskij" to "детский",
+            "multfilm" to "мультфильм",
+            "film-nuar" to "фильм-нуар",
+            "dlya-vzroslyh" to "для взрослых",
+            "dokumentalnyj" to "документальный",
+            "igra" to "игра",
+            "istoriya" to "история",
+            "koncert" to "концерт",
+            "korotkometrazhka" to "короткометражка",
+            "muzyka" to "музыка",
+            "myuzikl" to "мюзикл",
+            "novosti" to "новости",
+            "priklyucheniya" to "приключения",
+            "realnoe-tv" to "реальное ТВ",
+            "semejnyj" to "семейный",
+            "sport" to "спорт",
+            "tok-shou" to "ток-шоу",
+            "ceremoniya" to "церемония"
+    )
+}
