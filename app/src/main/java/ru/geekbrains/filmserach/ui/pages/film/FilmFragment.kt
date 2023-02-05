f1c1d0cbcd2683947fb8f087ec2e445af9423b42
anatoliy <anatoliy.kichuk@gmail.comgit config --global user.email anatoliy.kichuk@gmail.comgit config --global user.email anatoliy.kichuk@gmail.com>
2023-02-05 13:05:58 +0300

    Инкапсулированы ключи

--- app/src/main/java/ru/geekbrains/filmserach/ui/pages/film/FilmFragment.kt
+++ app/src/main/java/ru/geekbrains/filmserach/ui/pages/film/FilmFragment.kt
@@ -7,15 +7,13 @@ import android.view.ViewGroup
 import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.geekbrains.filmserach.R
 import ru.geekbrains.filmserach.data.PosterLoader
 import ru.geekbrains.filmserach.data.SELECTED_FILM
 import ru.geekbrains.filmserach.databinding.FragmentFilmBinding
 import ru.geekbrains.filmserach.domain.Film
-import ru.geekbrains.filmserach.ui.pages.map.MapsFragment
+import ru.geekbrains.filmserach.ui.pages.maps.MapsFragment
 import java.util.stream.Collectors
 
 class FilmFragment : Fragment() {
--- app/src/main/java/ru/geekbrains/filmserach/ui/pages/maps/MapsFragment.kt
+++ app/src/main/java/ru/geekbrains/filmserach/ui/pages/maps/MapsFragment.kt
@@ -1,14 +1,11 @@
-package ru.geekbrains.filmserach.ui.pages.map
-
-import androidx.fragment.app.Fragment
+package ru.geekbrains.filmserach.ui.pages.maps
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
