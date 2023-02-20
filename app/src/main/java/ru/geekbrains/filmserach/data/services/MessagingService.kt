e6020f21d155685a521bd8783e665c6114cd85ee
anatoliy <anatoliy.kichuk@gmail.comgit config --global user.email anatoliy.kichuk@gmail.comgit config --global user.email anatoliy.kichuk@gmail.com>
2023-02-20 18:54:26 +0300

    Добавлен сервис push-уведомлений

---
+++ app/src/main/java/ru/geekbrains/filmserach/data/services/MessagingService.kt
@@ -0,0 +1,75 @@
+package ru.geekbrains.filmserach.data.services
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import ru.geekbrains.filmserach.R
+
+class MessagingService : FirebaseMessagingService() {
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+
+        TODO("Отправить токен на сервер")
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+
+        val messageData = message.data
+
+        if (messageData.isNotEmpty()) {
+            handleDataMessage(messageData.toMap())
+        }
+    }
+
+    private fun handleDataMessage(data: Map<String, String>) {
+        val title = data[PUSH_KEY_TITLE]
+        val message = data[PUSH_KEY_MESSAGE]
+
+        if (!title.isNullOrBlank() && !message.isNullOrBlank()) {
+            showNotification(title, message)
+        }
+    }
+
+    private fun showNotification(title: String, message: String) {
+        val notificationBuilder = NotificationCompat
+            .Builder(applicationContext, CHANNEL_ID).apply {
+                setSmallIcon(R.drawable.notification_32)
+                setContentTitle(title)
+                setContentText(message)
+
+                priority = NotificationCompat.PRIORITY_DEFAULT
+            }
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel(notificationManager)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(notificationManager: NotificationManager) {
+        val name = "Channel description"
+        val descriptionText = "Channel description"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+            description = descriptionText
+        }
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    companion object {
+        private const val PUSH_KEY_TITLE = "title"
+        private const val PUSH_KEY_MESSAGE = "message"
+        private const val CHANNEL_ID = "channel_id"
+        private const val NOTIFICATION_ID = 37
+    }
+}
