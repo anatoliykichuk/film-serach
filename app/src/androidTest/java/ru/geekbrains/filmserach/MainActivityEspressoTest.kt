fc17113a0eb1c3b9578f656c870c6b95e00b0760
anatoliy <anatoliy.kichuk@gmail.comgit config --global user.email anatoliy.kichuk@gmail.comgit config --global user.email anatoliy.kichuk@gmail.com>
2023-10-18 20:13:13 +0300

    Добавлены инструментальные тесты

--- app/src/androidTest/java/ru/geekbrains/filmserach/MainActivityEspressoTest.kt
+++ app/src/androidTest/java/ru/geekbrains/filmserach/MainActivityEspressoTest.kt
@@ -1,23 +1,17 @@
 package ru.geekbrains.filmserach
 
-import androidx.navigation.testing.TestNavHostController
-import ru.geekbrains.filmserach.ui.MainActivity
-
 import androidx.test.core.app.ActivityScenario
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isChecked
+import androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import ru.geekbrains.filmserach.ui.pages.list.FilmListFragment
-
-//import androidx.fragment.app.testing.FragmentScenario
+import ru.geekbrains.filmserach.ui.MainActivity
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityEspressoTest {
@@ -35,10 +29,12 @@ class MainActivityEspressoTest {
     }
 
     @Test
-    fun navigateToFilmListFragment() {
-        //val navController = TestNavHostController(ApplicationProvider.getApplicationContext())
-        //val filmListScenario = launchFragmentInContainer<FilmListFragment>()
+    fun containerView_isDisplayed() {
+        onView(withId(R.id.main_menu)).check(matches(isDisplayed()))
+    }
 
-        onView(withId(R.id.film_list_fragment)).perform(click()).check(matches(isChecked()))
+    @Test
+    fun activityTextView_IsCompletelyDisplayed() {
+        onView(withId(R.id.main_menu)).check(matches(isCompletelyDisplayed()))
     }
 }
