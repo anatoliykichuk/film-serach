c09725792795741b8defb32932228c654a41078d
UpdateShu <rilabsure@yandex.ru>
2023-11-21 02:24:04 +0300

    Созданы 2 темы для приложения kinoPoiskTheme и limeTheme

---
+++ app/src/main/java/ru/geekbrains/filmserach/ui/pages/settings/SettingsFragment.kt
@@ -0,0 +1,22 @@
+package ru.geekbrains.filmserach.ui.pages.settings
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.geekbrains.filmserach.databinding.FragmentSettingsBinding
+
+class SettingsFragment : Fragment() {
+    private var _binding : FragmentSettingsBinding? = null
+    private val binding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
